316eb440d6b52d7aa731cdddbba8dc595d303c96
Gilwon Lee <gilwon.lee.dev@gmail.com>
2025-03-26 14:59:54 +0900

    refacot (#8)

--- proj1/sp_prj1_code/main.c
+++ proj1/sp_prj1_code/main.c
@@ -18,9 +18,9 @@
 bool QUIT = false;
 
 // Simple data structure manager
-struct list lists[MAX_STRUCTURE_COUNT];
-struct hash hashes[MAX_STRUCTURE_COUNT];
-//struct bitmap bitmaps[MAX_STRUCTURE_COUNT];
+struct list* lists[MAX_STRUCTURE_COUNT];
+struct hash* hashes[MAX_STRUCTURE_COUNT];
+struct bitmap* bitmaps[MAX_STRUCTURE_COUNT];
 
 static inline bool
 is_command_equal(char* input, char* command) {
@@ -107,20 +107,34 @@ void execute_command_from_buffer(char* buffer) {
 	// Create data strcuture
 	else if (is_command_equal(cmd, "create")) {
 		data_structure_index = get_ds_index(arg2);
-
-		if (strncmp(arg1, "list", 4) == 0) list_init (&lists[data_structure_index]);
-		else if (strncmp(arg1, "hashtable", 9) == 0) hash_init (&hashes[data_structure_index], hash_func, compare_hash_elements, NULL);
-		else if (strncmp(arg1, "bitmap", 6) == 0) {}
+		if (strncmp(arg1, "list", 4) == 0) {
+			lists[data_structure_index] = malloc(sizeof(struct list));
+			list_init (lists[data_structure_index]);
+		}
+		else if (strncmp(arg1, "hashtable", 9) == 0) {
+			hashes[data_structure_index] = malloc(sizeof(struct hash));
+			hash_init (hashes[data_structure_index], hash_func, compare_hash_elements, NULL);
+		}
+		else if (strncmp(arg1, "bitmap", 6) == 0) {
+			size_t bit_cnt = (size_t) atoi(arg3);
+			bitmaps[data_structure_index] = bitmap_create(bit_cnt);
+		}
 	}
 
 	// Delete data strcuture
 	else if (is_command_equal(cmd, "delete")) {
-		if (strncmp(arg1, "list", 4) == 0) {}
-		else if (strncmp(arg1, "hash", 4) == 0) {
-			data_structure_index = get_ds_index(arg1);
-			hash_destroy (&hashes[data_structure_index], NULL);
+		data_structure_index = get_ds_index(arg1);
+		if (strncmp(arg1, "list", 4) == 0) {
+			struct list* list = lists[data_structure_index];
+			while (!list_empty (list)) list_pop_front(list);
+			free (list);
 		}
-		else if (strncmp(arg1, "bm", 2)) {}
+		else if (strncmp(arg1, "hash", 4) == 0) {
+			struct hash* hash = hashes[data_structure_index];
+			hash_destroy (hash, NULL);
+			free (hash);
+		}
+		else if (strncmp(arg1, "bm", 2) == 0) {}
 	}
 
 	// Show data structure
@@ -128,26 +142,30 @@ void execute_command_from_buffer(char* buffer) {
 		data_structure_index = get_ds_index(arg1);
 
 		if (strncmp(arg1, "list", 4) == 0) {
-			if (&lists[data_structure_index] != NULL) {
+			struct list* list = lists[data_structure_index];
+			if (lists[data_structure_index] != NULL) {
 				struct list_elem *e;
-				for (e = list_begin (&lists[data_structure_index]); e != list_end (&lists[data_structure_index]); e = list_next (e)) {
+				for (e = list_begin (lists[data_structure_index]); e != list_end (lists[data_structure_index]); e = list_next (e)) {
 					struct list_item *i = list_entry (e, struct list_item, list_elem);
 					printf("%d ", i->data);
 				}
-				printf("\n");
 			}
 		}
 		else if (strncmp(arg1, "hash", 4) == 0) {
-			struct hash* h = &hashes[data_structure_index];
+			struct hash* h = hashes[data_structure_index];
 			struct hash_iterator* i = malloc(sizeof(struct hash_iterator));
 
 			hash_first (i, h);
 			while (hash_next (i)) printf("%d ", hash_cur(i)->data);
-			printf("\n");
 
 			free(i);
 		}
-		else if (strncmp(arg1, "bm", 2) == 0) {}
+		else if (strncmp(arg1, "bm", 2) == 0) {
+			struct bitmap* bitmap = bitmaps[data_structure_index];
+//			size_t i;
+//			for (i = 0; i < bitmap->bit_cnt; i++) printf("%d", bitmap->bits[elem_idx (i)]);
+		}
+		printf("\n");
 	}
 
 	// List related operations
@@ -158,7 +176,7 @@ void execute_command_from_buffer(char* buffer) {
 			int index = atoi(arg2);
 			int data = atoi(arg3);
 
-			struct list_elem *e = list_begin (&lists[data_structure_index]);
+			struct list_elem *e = list_begin (lists[data_structure_index]);
 			while (index) {e = list_next (e); index--;}
 
 			struct list_item* item_to_insert = init_list_item(data);
@@ -167,73 +185,73 @@ void execute_command_from_buffer(char* buffer) {
 		else if (is_command_equal(cmd, "list_insert_ordered")) {
 			int data = atoi(arg2);
 			struct list_item* item_to_insert = init_list_item(data);
-			list_insert_ordered (&lists[data_structure_index], &item_to_insert->list_elem, compare_elements, NULL);
+			list_insert_ordered (lists[data_structure_index], &item_to_insert->list_elem, compare_elements, NULL);
 		}
 		else if (is_command_equal(cmd, "list_push_front")) {
 			int data = atoi(arg2);
 			struct list_item* item_to_insert = init_list_item(data);
-			list_push_front (&lists[data_structure_index], &item_to_insert->list_elem);
+			list_push_front (lists[data_structure_index], &item_to_insert->list_elem);
 		}
 		else if (is_command_equal(cmd, "list_push_back")) {
 			int data = atoi(arg2);
 			struct list_item* item_to_insert = init_list_item(data);
-			list_push_back (&lists[data_structure_index], &item_to_insert->list_elem);
+			list_push_back (lists[data_structure_index], &item_to_insert->list_elem);
 		}
 		// Remove
 		else if (is_command_equal(cmd, "list_pop_front")) {
-			list_pop_front (&lists[data_structure_index]);
+			list_pop_front (lists[data_structure_index]);
 		}
 		else if (is_command_equal(cmd, "list_pop_back")) {
-			list_pop_back (&lists[data_structure_index]);
+			list_pop_back (lists[data_structure_index]);
 		}
 		else if (is_command_equal(cmd, "list_remove")) {
 			int index = atoi(arg2);
 
-			struct list_elem *e = list_begin (&lists[data_structure_index]);
+			struct list_elem *e = list_begin (lists[data_structure_index]);
 			while (index) {e = list_next (e); index--;}
 
 			list_remove (e);
 		}
 		// Peek
 		else if (is_command_equal(cmd, "list_front")) {
-			struct list_item *front = list_entry (list_front (&lists[data_structure_index]), struct list_item, list_elem);
+			struct list_item *front = list_entry (list_front (lists[data_structure_index]), struct list_item, list_elem);
 			printf("%d\n", front->data);
 		}
 		else if (is_command_equal(cmd, "list_back")) {
-			struct list_item *back = list_entry (list_back (&lists[data_structure_index]), struct list_item, list_elem);
+			struct list_item *back = list_entry (list_back (lists[data_structure_index]), struct list_item, list_elem);
 			printf("%d\n", back->data);
 		}
 		else if (is_command_equal(cmd, "list_empty")) {
-			bool is_empty = list_empty (&lists[data_structure_index]);
+			bool is_empty = list_empty (lists[data_structure_index]);
 			printf("%s\n", is_empty ? "true" : "false");
 		}
 		else if (is_command_equal(cmd, "list_size")) {
-			size_t size = list_size (&lists[data_structure_index]);
+			size_t size = list_size (lists[data_structure_index]);
 			printf("%zu\n", size);
 		}
 		else if (is_command_equal(cmd, "list_max")) {
-			struct list_item* max_item = list_entry (list_max (&lists[data_structure_index], compare_elements, NULL), struct list_item, list_elem);
+			struct list_item* max_item = list_entry (list_max (lists[data_structure_index], compare_elements, NULL), struct list_item, list_elem);
 			printf("%d\n", max_item->data);
 		}
 		else if (is_command_equal(cmd, "list_min")) {
-			struct list_item* min_item = list_entry (list_min (&lists[data_structure_index], compare_elements, NULL), struct list_item, list_elem);
+			struct list_item* min_item = list_entry (list_min (lists[data_structure_index], compare_elements, NULL), struct list_item, list_elem);
 			printf("%d\n", min_item->data);
 		}
 		// Sort
 		else if (is_command_equal(cmd, "list_reverse")) {
-			list_reverse (&lists[data_structure_index]);
+			list_reverse (lists[data_structure_index]);
 		}
 		else if (is_command_equal(cmd, "list_sort")) {
-			list_sort (&lists[data_structure_index], compare_elements, NULL);
+			list_sort (lists[data_structure_index], compare_elements, NULL);
 		}
 		else if (is_command_equal(cmd, "list_unique")) {
 			struct list* duplicates = NULL;
 			if (args_count >= 2) {
 				int duplicates_index = get_ds_index(arg2);
-				duplicates = &lists[duplicates_index];
+				duplicates = lists[duplicates_index];
 			}
 
-			list_unique (&lists[data_structure_index], duplicates, compare_elements, NULL);
+			list_unique (lists[data_structure_index], duplicates, compare_elements, NULL);
 		}
 		else if (is_command_equal(cmd, "list_swap")) {
 			struct list_elem* first = NULL;
@@ -243,7 +261,7 @@ void execute_command_from_buffer(char* buffer) {
 			int second_index = atoi(arg3);
 
 			int curr = 0;
-			struct list_elem *e =list_begin (&lists[data_structure_index]);
+			struct list_elem *e =list_begin (lists[data_structure_index]);
 			while (first == NULL || second == NULL) {
 				if (curr == first_index) first = e;
 				else if (curr == second_index) second = e;
@@ -260,26 +278,24 @@ void execute_command_from_buffer(char* buffer) {
 			int index_to_slice_from = atoi(arg4);
 			int index_to_slice_to = atoi(arg5);
 
-			struct list_elem *before = list_begin (&lists[list_index_to_in]);
+			struct list_elem *before = list_begin (lists[list_index_to_in]);
 			while (index_to_in) {before = list_next (before); index_to_in--;}
 
-			struct list_elem *slice_from = list_begin (&lists[list_index_to_slice]);
+			struct list_elem *slice_from = list_begin (lists[list_index_to_slice]);
 			while (index_to_slice_from) {slice_from = list_next (slice_from); index_to_slice_from--;}
 
-			struct list_elem *slice_to = list_begin (&lists[list_index_to_slice]);
+			struct list_elem *slice_to = list_begin (lists[list_index_to_slice]);
 			while (index_to_slice_to) {slice_to = list_next (slice_to); index_to_slice_to--;}
 
 			list_splice(before, slice_from, slice_to);
 		}
-		else if (is_command_equal(cmd, "list_shuffle")) {
-			list_suffle(&lists[data_structure_index]);
-		}
+		else if (is_command_equal(cmd, "list_shuffle")) list_suffle(lists[data_structure_index]);
 	}
 
 	// Hash related operations
 	else if (is_hash_command(cmd)) {
 		data_structure_index = get_ds_index(arg1);
-		struct hash* hash = &hashes[data_structure_index];
+		struct hash* hash = hashes[data_structure_index];
 
 		// Insert
 		if (is_command_equal(cmd, "hash_insert")) {
